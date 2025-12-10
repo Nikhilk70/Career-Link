76203acc4acb22542dc33d9c9d3b6d0ec37dfb45
Nikhilk70 <nikhilksiva70@gmail.com>
2025-12-10 22:18:53 +0530

    quotes added

--- Job Portal/src/components/Home/About.jsx
+++ Job Portal/src/components/Home/About.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import logo from "../../assets/Images/Logo-T.png";
 
 function About() {
   return (
-    <div className='bg-amber-50 h-[100dvh] flex flex-col justify-center items-center'>
-      <div>
-        <h2>Why Choose Career Link</h2>
+    <div className='flex aboutSection'>
+      <div className='aboutText h-[100dvh] flex flex-col '>
+        <div className='heading'>
+          <img className='' src={logo} alt="Not Available" />
+          <h2>Why Choose Career Link</h2>
+        </div>
+        <div className='aboutList'>
+          <ul>
+            <li><b>Smart Job Matching:</b> AI-powered recommendations for faster hiring.</li>
+            <li><b>Verified Employers:</b> Trusted companies with real opportunities.</li>
+            <li><b>Easy Applications:</b> Apply with one click and track your status.</li>
+            <li><b>Remote & Local Jobs:</b> Work from anywhere or nearby.</li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <ul>
-          <li><b>Smart Job Matching:</b> AI-powered recommendations for faster hiring.</li>
-          <li><b>Verified Employers:</b> Trusted companies with real opportunities.</li>
-          <li><b>Easy Applications:</b> Apply with one click and track your status.</li>
-          <li><b>Remote & Local Jobs:</b> Work from anywhere or nearby.</li>
-        </ul>
+      <div className='aboutVideo flex items-center justify-center'>
+        <video
+          src="https://res.cloudinary.com/dxjdraawa/video/upload/v173183/videoplayback_kdxk91.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="w-full h-auto"
+        ></video>
       </div>
     </div>
   )
--- Job Portal/src/components/Home/Review.jsx
+++ Job Portal/src/components/Home/Review.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import steve from "../../assets/cmpy_logos/SteveJobs.jpeg";
 
 function Review() {
   return (
-    <div>Review</div>
+    <div className='review-container'>
+      <div className='quotesSection flex gap-[50px] justify-center items-center '>
+        <div className='quotesText h-full flex flex-col justify-center items-center'>
+          <h2 className='font-bold text-[20px]'>"Your time is limite, so don't waste is living someone else's life."</h2>
+          <p className='w-full text-right mr-5'>- Steve Jobs</p>
+        </div>
+        <img src={steve} alt="" />
+      </div>
+      <h1>This is Review section</h1>
+    </div>
   )
 }
 
