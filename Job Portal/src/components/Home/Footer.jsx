d8a94265930c598b2b73f370d904b3015f3cc1de
Nikhilk70 <nikhilksiva70@gmail.com>
2026-01-03 22:16:04 +0530

    Last Update 03/03

--- Job Portal/src/components/Home/Footer.jsx
+++ Job Portal/src/components/Home/Footer.jsx
@@ -7,6 +7,28 @@ function Footer() {
         <h1>Career Link</h1>
         <p>All in One Place</p>
       </div>
+      <div>
+        <div className='footerAbout'>
+          <h2>About Us</h2>
+          <ul>
+            <li><b>Smart Job Matching:</b> AI-powered recommendations for faster hiring.</li>
+            <li><b>Verified Employers:</b> Trusted companies with real opportunities.</li>
+            <li><b>Easy Applications:</b> Apply with one click and track your status.</li>
+            <li><b>Remote & Local Jobs:</b> Work from anywhere or nearby.</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Career Resources</h2>
+          <ul>
+            <li>Career Aptitude Test</li>
+            <li>Examples of Strengths and Weaknesses for Job Interviews</li>
+            <li>High-Income Skills to Learn</li>
+            <li>How Does Cryptocurrency Work?</li>
+            <li>How to Highlight Duplicates in Google Sheets</li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
